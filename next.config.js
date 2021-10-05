893982dde86aaeb6868342f2c1c6b118017b3280
Felipe Ospina <pipe.98.oh@gmail.com>
2021-10-05 05:30:24 -0500

    feat: env & deploy config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+const dotenv = require('dotenv')
+const path = require('path')
+
 module.exports = {
     webpack(config) {
         config.module.rules.push({
@@ -9,4 +12,17 @@ module.exports = {
         return config;
     },
     reactStrictMode: true,
-}
+    env: process.env.NEXT_ENV === 'dev'
+        ? dotenv
+            .config({ path: path.join(__dirname, '.env.development.local')})
+            .parsed || {}
+        : process.env.NEXT_ENV === 'test'
+            ? dotenv
+                .config({ path: path.join(__dirname, '.env.test.local')})
+                .parsed || {}
+            : process.env.NEXT_ENV === 'prod'
+                ? dotenv
+                    .config({ path: path.join(__dirname, '.env.production.local')})
+                    .parsed || {}
+                : {},
+};
