bb05dd2536f45b0bca61ec8b1b6f999e3aa4f887
Felipe Ospina <pipe.98.oh@gmail.com>
2021-10-11 12:33:27 -0500

    fix: time pickers

--- src/components/atoms/HourHand.tsx
+++ src/components/atoms/HourHand.tsx
@@ -1,36 +1,36 @@
-import React, {useState, Fragment} from "react";
-import {
-    Typography
-} from "@material-ui/core";
+import React, { useState } from 'react';
+import { Typography } from '@material-ui/core';
 
-//from https://material-ui-pickers.dev/demo/timepicker
-import { TimePicker, KeyboardTimePicker } from "@material-ui/pickers";
+// from https://material-ui-pickers.dev/demo/timepicker
+import { KeyboardTimePicker } from '@material-ui/pickers';
 
 function HourHand() {
-  const [selectedStartTime, handleStartTimeChange] = useState<Date | null>(
-    new Date('2021-10-09T21:11:54'),);
-  const [selectedEndTime, handleEndTimeChange] = useState<Date | null>(
-    new Date('2021-10-09T21:11:54'),);
+	const [selectedStartTime, handleStartTimeChange] = useState<Date | null>(
+		new Date('2021-10-09T21:11:54'),
+	);
+	const [selectedEndTime, handleEndTimeChange] = useState<Date | null>(
+		new Date('2021-10-09T21:11:54'),
+	);
 
-  return (
-    <Fragment>
-        <Typography variant="h6">Horas</Typography>
-        <KeyboardTimePicker
-        ampm={false}
-        variant="inline"
-        label="Hora de inicio"
-        value={selectedStartTime}
-        onChange={handleStartTimeChange}
-        />
-        <KeyboardTimePicker
-        ampm={false}
-        variant="inline"
-        label="Hora de finalización"
-        value={selectedEndTime}
-        onChange={handleEndTimeChange}
-        />
-    </Fragment>
-  );
+	return (
+		<>
+			<Typography variant="h6">Horas</Typography>
+			<KeyboardTimePicker
+				ampm={false}
+				variant="inline"
+				label="Hora de inicio"
+				value={selectedStartTime}
+				onChange={handleStartTimeChange}
+			/>
+			<KeyboardTimePicker
+				ampm={false}
+				variant="inline"
+				label="Hora de finalización"
+				value={selectedEndTime}
+				onChange={handleEndTimeChange}
+			/>
+		</>
+	);
 }
 
 export default HourHand;
