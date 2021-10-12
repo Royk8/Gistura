2985528a5c27e9db7fb92d581b1f3a6e8e689f17
Felipe Ospina <pipe.98.oh@gmail.com>
2021-10-12 12:27:05 -0500

    fix: category hook now return label and color specs

--- src/hooks/Category.tsx
+++ src/hooks/Category.tsx
@@ -1,27 +1,66 @@
+import React, { FC } from 'react';
 import * as Icons from '@material-ui/icons';
-import { SvgIcon } from '@material-ui/core';
+import { SvgIcon, Theme, Typography } from '@material-ui/core';
 
+import { makeStyles } from '@material-ui/styles';
 import * as CustomIcons from '../components/atoms/icons/MapIcons';
 import { useAppSelector } from './redux';
 import { selectCategories } from '../state/slices/categorySlice';
 import { Icon as IconInterface } from '../interfaces/iconInterfaces';
 
-const useCategory = (id: number) => {
+interface Props {
+	id: number;
+	label?: boolean;
+	noColor?: boolean;
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+	container: {
+		display: 'flex',
+		alignItems: 'center',
+		'&>*:not(:last-child)': {
+			marginRight: theme.spacing(1),
+		},
+	},
+}));
+
+const useCategory = ({ id, label, noColor }: Props) => {
 	const { categories } = useAppSelector(selectCategories);
 
 	const category = categories.find(({ id: findId }) => findId === id);
+	const classes = useStyles();
 
 	if (!category) return null;
 
 	const { name, type } = category.icon;
 
-	const IconComponent: typeof SvgIcon =
+	const Icon: typeof SvgIcon =
 		type === IconInterface.Types.CUSTOM
 			? (CustomIcons as any)[name]
 			: (Icons as any)[name];
 
+	const Container: FC = ({ children }) =>
+		label ? (
+			<div className={classes.container}>{children}</div>
+		) : (
+			<>{children}</>
+		);
+
+	const style = noColor ? undefined : { color: category.color };
+
+	const Component = () => (
+		<Container>
+			<Icon color="inherit" style={style} />
+			{label && (
+				<Typography color="inherit" style={style}>
+					{category.name}
+				</Typography>
+			)}
+		</Container>
+	);
+
 	return {
-		IconComponent,
+		Component,
 		category,
 	};
 };
